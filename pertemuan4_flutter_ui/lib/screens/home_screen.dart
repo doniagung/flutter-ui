831b9801f17c1a9987cf60f35dfe4493134fa45e
doniagung <agungdoni823@gmail.com>
2024-05-16 21:48:59 +0700

    HOMESCREEN

--- pertemuan4_flutter_ui/lib/screens/home_screen.dart
+++ pertemuan4_flutter_ui/lib/screens/home_screen.dart
@@ -11,123 +11,307 @@ class HomeScreen extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
 
     return Scaffold(
-      body: SingleChildScrollView(child: Column(
+        body: SingleChildScrollView(
+      child: Column(
         children: [
           Stack(
             children: [
-              Image.asset('assets/images/background.png', height: 241,
-              fit: BoxFit.cover,
-              width: double.infinity,),
-
+              Image.asset(
+                'assets/images/background.png',
+                height: 241,
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
               Padding(
                 padding: const EdgeInsets.only(top: 74, left: 24, right: 24),
-                child: Column(
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text('Jobify.', 
+                child: Column(children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Jobify.',
                         style: GoogleFonts.plusJakartaSans(
-                          fontSize: 22, 
-                          fontWeight: FontWeight.w800,
-                          color: Colors.white 
-                          ),
-                         ),
-                         const Spacer(),
-                         SvgPicture.asset('assets/svgs/ic_notification.svg'),
-                         const SizedBox(
-                          width: 15,
-                         ),
-                         Container(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white),
+                      ),
+                      const Spacer(),
+                      SvgPicture.asset('assets/svgs/ic_notification.svg'),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Container(
                           width: 42,
                           height: 42,
                           clipBehavior: Clip.hardEdge,
                           decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(21)
+                              borderRadius: BorderRadius.circular(21)),
+                          child: Image.network(
+                              'https://gravatar.com/avatar/27205e5c51cb03f862138b22bcb5dc20f94a342e744ff6df1b8dc8af3c865109.jpg'))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(14),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Column(children: [
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 6, top: 6, right: 6),
+                        child: Row(children: [
+                          SvgPicture.asset('assets/svgs/ic_search.svg'),
+                          const SizedBox(
+                            width: 10,
                           ),
-                          child:  Image.network('https://gravatar.com/avatar/27205e5c51cb03f862138b22bcb5dc20f94a342e744ff6df1b8dc8af3c865109.jpg'))
+                          Text(
+                            'Search job, company etc',
+                            style: GoogleFonts.plusJakartaSans(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: const Color(0xFF7F879E)),
+                          ),
+                        ]),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Divider(
+                        color: Color(0XFFF9F9F9),
+                        height: 1.5,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 6, right: 6),
+                        child: Row(children: [
+                          SvgPicture.asset('assets/svgs/ic_location.svg'),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Location',
+                            style: GoogleFonts.plusJakartaSans(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: const Color(0xFF7F879E)),
+                          ),
+                        ]),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () => {},
+                        child: Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(14),
+                          decoration: BoxDecoration(
+                              color: Color(0xFF3860E2),
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Center(
+                            child: Text(
+                              'Button',
+                              style: GoogleFonts.plusJakartaSans(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      )
+                    ]),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Container(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Suggested Job',
+                          style: GoogleFonts.plusJakartaSans(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w800,
+                              color: Color(0xFF1B2124)),
+                        ),
+                        const Spacer(),
+                        Text(
+                          'See All',
+                          style: GoogleFonts.plusJakartaSans(
+                              fontSize: 14, color: Color(0xFF7F879E)),
+                        )
                       ],
                     ),
-                    const SizedBox(
-                  height: 30,
-                 ),
-                 Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.all(14),
-                  decoration: BoxDecoration(
-                    color: Colors.white, 
-                    borderRadius: BorderRadius.circular(12)),
-                    child: Column(
-                      children: [
-                        Padding(padding: 
-                        const EdgeInsets.only(left: 6, top: 6, right: 6),
-                        child: Row(
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(14),
+                    decoration: BoxDecoration(color: Colors.white,
+                    borderRadius: BorderRadius.circular(8)),
+                    child: Column(children: [
+                      Padding(padding: const EdgeInsets.only(left: 6, right: 6, top: 6),
+                      child: Row(children: [
+                        SvgPicture.asset('assets/svgs/ic_pinterest.svg'),
+                       Padding(padding: const EdgeInsets.only(left: 20)),
+                        Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            SvgPicture.asset('assets/svgs/ic_search.svg'),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text('Search job, company etc', 
-                            style: GoogleFonts.plusJakartaSans(
-                              fontSize: 14, 
-                              fontWeight: FontWeight.w500, 
-                              color: const Color(0xFF7F879E) 
-                              ),),
-                          ]
+                            Text('Pinterest', style: GoogleFonts.plusJakartaSans(fontSize: 14, fontWeight: FontWeight.w800),),
+                            Text('Senior UI/UX Designer', style: GoogleFonts.plusJakartaSans(fontSize: 12, color: Color(0xFF7F879E))),
+                          ],
                         ),
+                        const Spacer(),
+                        SvgPicture.asset('assets/svgs/ic_save.svg'),
+                        const SizedBox(
+                          height: 20,
                         ),
-                          const SizedBox(
-                              height: 20,
-                            ),
-                         const Divider(
-                                color: Color(0XFFF9F9F9),
-                                height: 1.5,
-                              ),
-                               const SizedBox(
-                              height: 20,
-                            ),
-                             Padding(padding: 
-                        const EdgeInsets.only(left: 6, right: 6),
+                        
+                      ],
+                      ),
+                      
+                      ),
+                      Padding(padding: 
+                        const EdgeInsets.only(left: 15, right: 20, top: 20),
                         child: Row(
                           children: [
                             SvgPicture.asset('assets/svgs/ic_location.svg'),
                             const SizedBox(
-                              width: 10,
-                            ),
-                            Text('Location', 
+                            width: 10,
+                          ),
+                          Text(
+                            'California, Freelance ,WFO',
                             style: GoogleFonts.plusJakartaSans(
-                              fontSize: 14, 
-                              fontWeight: FontWeight.w500, 
-                              color: const Color(0xFF7F879E) 
-                              ),),
-                          ]
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                                color: const Color(0xFF7F879E)),
+                          ),
+                          ],
                         ),
                         ),
-                        const SizedBox(
-                              height: 20,
+                        Padding(padding: 
+                        const EdgeInsets.only(left: 15, right: 20, top: 20),
+                        child: Row(
+                          children: [
+                            SvgPicture.asset('assets/svgs/ic_dolar.svg'),
+                            const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            '25,000 dollars ',
+                            style: GoogleFonts.plusJakartaSans(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w700,
+                                color: const Color(0xFF1B2124)),
+                          ),
+                          Text(
+                            '/Month',
+                            style: GoogleFonts.plusJakartaSans(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                                color: const Color(0xFF7F879E)),
+                          ),
+                          ],
+                        ),
+                        ),
+                         const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 14, right: 14, ),
+                        child: Row(
+                          children: [
+                            Text('Senior UI/UX Designer needed, for collaborate with team and developer as full time designer. by having good communication skills,', 
+                            style: GoogleFonts.plusJakartaSans(
+                              fontSize: 12, fontWeight: FontWeight.w500, color: Color(0xFF7F879E)
+                            ) ,)
+                          ],
+                        ),
+                      ),
+                       const SizedBox(
+                        height: 20,
+                      ),
+                        GestureDetector(
+                        onTap: () => {},
+                        child: Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(14),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(color: Color(0xFF3860E2),
+                              width: 2)),
+                          child: Center(
+                            child: Text(
+                              'Apply Now',
+                              style: GoogleFonts.plusJakartaSans(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF3860E2),
+                                  ),
                             ),
-                            GestureDetector(
-                              onTap: () => {},
-                              child: Container(
-                                width: double.infinity,
-                                padding: const EdgeInsets.all(14),
-                                decoration: 
-                                 BoxDecoration(color: Color(0xFF3860E2), borderRadius: BorderRadius.circular(8)),
-                                child: Center(child: Text('Button',
-                                style: GoogleFonts.plusJakartaSans(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white),),),
-                              ),
-                            )
-                      ]
+                          ),
+                        ),
+                      ),
+                    ],
                     ),
-                 )
-                  ],
+                  )
+                ],
                 ),
-              )
+              ),
             ],
           )
         ],
-        ),
-      )
+      ),
+      
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        onTap: (int index) {
+          // Handle bottom navigation item tap
+        },
+        items:  <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/svgs/ic_home.svg'
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/svgs/ic_saved.svg'
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/svgs/ic_apply.svg'
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/svgs/ic_masage.svg'
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/svgs/ic_profil.svg'
+              ),
+              label: 'Home',
+            ),
+        ],
+      ),
     );
   }
 }
